4889140aa464f3987d7699fe2c4d412d12d9d1fc
dzhong <dzhong@covariant.ai>
2025-07-24 17:05:13 +0800

    add resize

--- data/grasp_dataset.py
+++ data/grasp_dataset.py
@@ -4,14 +4,16 @@ import pandas as pd
 from io import BytesIO
 import os
 import boto3
+import torchvision.transforms as transforms
 
 class RGBD_R7_Dataset(torch.utils.data.Dataset):
-    def __init__(self, s3_path: str, split: str):
+    def __init__(self, s3_path: str, split: str, resize: tuple=(224, 224)):
         self.s3_path = s3_path
         table = pd.read_parquet(self.s3_path)
         self.split = split
         self.table = table[table["split"] == self.split]
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.resize_transform = transforms.Resize(resize)
     
     def __len__(self):
         return len(self.table)
@@ -22,8 +24,8 @@ class RGBD_R7_Dataset(torch.utils.data.Dataset):
         # load the data from s3
         s3_client = boto3.client('s3')
         datum = {
-            "obj_depth_map": torch.from_numpy(load_np_s3(s3_links["obj_depth_map"], s3_client)),
-            "obj_rgb": torch.from_numpy(load_np_s3(s3_links["obj_rgb"], s3_client)),
+            "obj_depth_map": self.resize_transform(torch.from_numpy(load_np_s3(s3_links["obj_depth_map"], s3_client)).unsqueeze(0)),
+            "obj_rgb": self.resize_transform(torch.from_numpy(load_np_s3(s3_links["obj_rgb"], s3_client))),
             "top_grasp_r7": torch.from_numpy(load_np_s3(s3_links["top_grasp_r7"], s3_client)),
         }
 
